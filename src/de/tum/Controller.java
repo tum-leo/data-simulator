b942efe93e42b473be9bcbedd8c5e561bf2b51c7
doreuter <do_reuter@web.de>
2017-12-09 22:57:08 +0100

    Project Structure V2

--- src/de/tum/Controller.java
+++ src/de/tum/Controller.java
@@ -1,7 +1,33 @@
 package de.tum;
 
+import de.tum.datamodel.DataModel;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Controller {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
     public static void main(String[]args){
-        System.out.println("Hello World");
+
+        Date date = new Date();
+        System.out.println("---Start: " + sdf.format(date) + "---");
+
+        Controller controller = new Controller();
+
+        controller.control();
+
+        date = new Date();
+        System.out.println("---Ende: " + sdf.format(date) + "---");
+    }
+
+    public void control(){
+
+    }
+
+    public DataModel createDataModel(){
+        return new DataModel();
     }
 }
--- src/de/tum/datamodel/DataModel.java
+++ src/de/tum/datamodel/DataModel.java
@@ -1,5 +1,29 @@
 package de.tum.datamodel;
 
+import de.tum.datamodel.Tables.*;
+
+import java.util.ArrayList;
+
 public class DataModel {
 
+    ArrayList<Object> dataModel;
+
+    public DataModel(){
+        this.dataModel = createTables();
+    }
+
+    private ArrayList<Object> createTables(){
+
+        ArrayList<Object> tables = new ArrayList<Object>();
+
+        tables.add(new Fleet());
+        tables.add(new Sensors());
+        tables.add(new SensorData());
+        tables.add(new Customer());
+        tables.add(new RentLog());
+        tables.add(new RepairLog());
+        tables.add(new Station());
+
+        return tables;
+    }
 }
