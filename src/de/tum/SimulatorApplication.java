58d85015890fe6c780a1a2f0de6eb4bcb50604dc
Tobias Bodor <tobias.bodor@subseq.net>
2017-12-17 10:09:53 +0100

    Add pom.xml: Make project a Maven project, inherit from SpringBoot; add SimulatorApplication; delete Controller

---
+++ src/de/tum/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package de.tum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatorApplication.class);
+    }
+
+}
